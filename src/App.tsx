d3210e39e693fffad19889078de02310520ec57c
Shriyansh-SRT <shriyanshraj.timsina@gmail.com>
2025-02-23 00:23:16 +0545

    add pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import SigninForm from "./_auth/form/SigninForm"
 import SignupForm from "./_auth/form/SignupForm"
 import AuthLayout from "./_auth/AuthLayout"
 import RootLayout from "./_root/RootLayout"
-import { Home } from "./_root/pages/index"
+import { Home, Explore, Saved, AllUsers, CreatePost, UpdateProfile, EditPost, PostDetails, Profile, LikedPosts } from "./_root/pages/index"
 import './index.css'
 import { Toaster } from "@/components/ui/toaster"
 
@@ -18,7 +18,16 @@ const App = () => {
 
           <Route element={<RootLayout />}>
             <Route index element={<Home />} />
+            <Route path="/explore" element={<Explore />} />
+            <Route path="/saved" element={<Saved />} />
+            <Route path="/all-users" element={<AllUsers />} />
+            <Route path="/create-post" element={<CreatePost />} />
+            <Route path="/update-post/:id" element={<EditPost />} />
+            <Route path="/posts/:id" element={<PostDetails />} />
+            <Route path="/profile/:id/*" element={<Profile />} />
+            <Route path="/update-profile/:id" element={<UpdateProfile />} />
           </Route>
+            <Route path="/liked-posts" element={<LikedPosts />} />
       </Routes>
 
       <Toaster />
